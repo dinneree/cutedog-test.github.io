0105062f3b90851a9b40aa76817d7cdd7f05fb27
dinneree <111102513+dinneree@users.noreply.github.com>
2022-08-13 20:16:06 +0900

    second file
    good

---
+++ App.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+import {StyleSheet} from 'react-native';
+export default function App() {
+  return (
+    <WebView 
+      style={styles.container}
+      source={{ uri: 'https://dinneree.githhub.io' }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{ 
+        marginTop:20
+        
+    }
+
+
+
+});
